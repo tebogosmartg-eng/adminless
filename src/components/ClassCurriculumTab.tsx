3675449e6a1bb7c465211c8e261dd46435106b0f
[dyad] <git@dyad.sh>
2026-02-11 18:46:58 +0200

    [dyad] Reverting recent code changes - wrote 5 file(s)

--- src/components/ClassCurriculumTab.tsx
+++ src/components/ClassCurriculumTab.tsx
@@ -1,4 +1,4 @@
-character in JSX text to fix compilation error.">
+character in JSX text to fix the compilation error.">
 "use client";
 
 import { useCurriculumProgress } from "@/hooks/useCurriculumProgress";
